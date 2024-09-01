a9ccbc2c9b7efefbba221c77912be0f597824d0c
lucasFelixSilveira <lucasdwbfff@gmail.com>
2024-09-01 03:18:42 -0300

    first commit :tada:

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# bfinancial-nodejs
+> [!WARNING]
+> Se pretende trabalhar com linguagens de alto nível (Js ou Ts), recomendamos fortemente a lib de [TypeScript]() para outro runtime fora o Node.js. Como, Bun, ou Deno.
+
+```js
+const client = new Client().login("admin");
+const payment = client.payments;
+
+const info = {
+  amount: 2,
+  payer_email: "test@gmail.com"
+};
+
+/*
+  Gera o pagamento pix, e envia para o data o conteúdo
+  necessário para que seja possível o cliente efetuar o 
+  pagamento.
+*/
+payment.create(new Transaction(info).pix()).then(pix => {;
+  console.log(pix);
+}).catch(payment_error => {
+  console.log(payment_error)
+})
+```
---
+++ index.js
@@ -0,0 +1,73 @@
+/**
+ * By. Lucas Silveira <contato.lucasdwbfff@gmail.com>
+ */
+
+class Client {
+  constructor() {}
+  /**
+   * 
+   * @param {string} api_key your API-Key
+   * @returns {object} PaymentInterface type
+   */
+  login(api_key) {
+    return { payments: Payment(api_key) };
+  }
+}
+
+class Payment {
+  constructor(api_key) {
+    this.__api_url = "http://127.0.0.1:8080";
+    this.__api_key = api_key;
+  }
+
+  create(payment_data) {
+    return new Promise(async (resolve, reject) => {
+      const request = await fetch(`${this.__api_url}/payment/create`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization-key": this.__api_key
+        },
+        body: JSON.stringify(payment_data)
+      });
+
+      const response = await request.json();
+      (data.status == 200 ? resolve : reject)(response.data);
+    });
+  }
+}
+
+class Transaction {
+  constructor(info) {
+    this.info = info;
+  }
+
+  pix() {
+    const data = this.info;
+    return { 
+      method: "Pix",
+      amount: data.amount,
+      payer_email: data.payer_email
+    }
+  }
+  
+  card() {
+    const data = this.info;
+    return { 
+      method: "Card",
+
+      amount: data.amount,
+      cvv: data.cvv,
+      number: daat.number,
+      
+      payer_cpf: data.payer_cpf,
+      payer_email: data.payer_email,
+      payer_name: data.payer_name,
+      
+      expiration_month: data.expiration_month,
+      expiration_year: data.expiration_year,
+    }
+  }
+}
+
+export { Client, Transaction };
